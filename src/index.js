812fb3306c27f899c6047e628b46e26d5d36ff30
Sergey Lebedinsky <sergey.lebedinsky@upsilonit.com>
2019-03-17 20:09:25 +0300

    feat: add solution for love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  let num2, num3; 
+  let arr = preferences.slice();
+
+  arr.unshift('Element for shifting array index (from 0 to 1)');
+
+  for (let i = 1; i < arr.length; i++) {
+    num2 = arr[i];
+    num3 = arr[num2];
+
+    if (num2 !== num3 && 
+        arr[i] === num2 && 
+        arr[num2] === num3 && 
+        arr[num3] === i) {
+    	count++;
+    	delete(arr[i]);
+    }
+  }
+
+  return count;
 };
